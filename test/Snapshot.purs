5a40769054d169222ec5783256405ba502687c1f
Christoph Hegemann <christoph.hegemann1337@gmail.com>
2023-10-09 22:22:47 +0200

    Fixes test suite for the breaking changes to node packages (#11)

--- test/Snapshot.purs
+++ test/Snapshot.purs
@@ -9,7 +9,6 @@ import Data.Array as Array
 import Data.Either (Either(..))
 import Data.Foldable (for_)
 import Data.Maybe (Maybe(..))
-import Data.Posix.Signal (Signal(..))
 import Data.String (Pattern(..))
 import Data.String as String
 import Data.String.CodeUnits (stripSuffix)
@@ -21,9 +20,10 @@ import Effect.Class (liftEffect)
 import Node.Buffer (Buffer, freeze)
 import Node.Buffer as Buffer
 import Node.Buffer.Immutable as ImmutableBuffer
-import Node.ChildProcess (ExecResult, defaultExecOptions)
+import Node.ChildProcess (ExecResult)
 import Node.ChildProcess as ChildProcess
 import Node.Encoding (Encoding(..))
+import Node.Errors.SystemError as SystemError
 import Node.FS.Aff (readFile, readdir, writeFile)
 import Node.Path (basename)
 import Node.Path as Path
@@ -75,7 +75,7 @@ snapshotMainOutput directory accept mbPattern = do
     result <- exec $ "node --input-type=module -e 'import { main } from \"./output/" <> name <> "/index.js\";main()'"
     case result of
       { error: Just err } ->
-        throwError err
+        throwError (SystemError.toError err)
       { stdout } -> do
         output <- liftEffect $ bufferToUTF8 stdout
         let
@@ -101,15 +101,15 @@ snapshotMainOutput directory accept mbPattern = do
 
 exec :: String -> Aff ExecResult
 exec command = makeAff \k -> do
-  childProc <- ChildProcess.exec command defaultExecOptions (k <<< pure)
-  pure $ effectCanceler $ ChildProcess.kill SIGABRT childProc
+  childProc <- ChildProcess.exec' command identity (k <<< Right)
+  pure $ effectCanceler $ void (ChildProcess.kill childProc)
 
 execWithStdin :: String -> String -> Aff ExecResult
 execWithStdin command input = makeAff \k -> do
-  childProc <- ChildProcess.exec command defaultExecOptions (k <<< pure)
-  _ <- Stream.writeString (ChildProcess.stdin childProc) UTF8 input mempty
-  Stream.end (ChildProcess.stdin childProc) mempty
-  pure $ effectCanceler $ ChildProcess.kill SIGABRT childProc
+  childProc <- ChildProcess.exec' command identity (k <<< Right)
+  _ <- Stream.writeString (ChildProcess.stdin childProc) UTF8 input
+  Stream.end (ChildProcess.stdin childProc)
+  pure $ effectCanceler $ void (ChildProcess.kill childProc)
 
 bufferToUTF8 :: Buffer -> Effect String
 bufferToUTF8 = map (ImmutableBuffer.toString UTF8) <<< freeze
